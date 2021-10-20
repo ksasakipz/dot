5323e23e60b7fd9425ba24008402ff57da8a885d
ksasakipz <kevinmsasaki@gmail.com>
2021-10-19 22:27:52 -0700

    Added to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
-cp ./.vimrc ~/.vimrc
-cp ./.tmux.conf ~/.tmux.conf
+echo 
+echo About to copy dot files &&
+	cp ./.vimrc ~/.vimrc &&
+	echo Copied .vimrc &&
+	cp ./.tmux.conf ~/.tmux.conf &&
+	echo Copied .tmux.conf
